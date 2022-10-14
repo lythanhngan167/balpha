c35c268906a461ffa257946ec4612acc2635deed
Ngan Ly <lythanhngan167@gmail.com>
2022-10-14 11:59:54 +0700

    css

--- templates/protostar/index.php
+++ templates/protostar/index.php
@@ -47,7 +47,7 @@ else
 JHtml::_('bootstrap.framework');
 // Add Stylesheets
 //JHtml::_('stylesheet', 'template.css', array('version' => 'auto', 'relative' => true));
-$version = '114072022';
+$version = '114102022';
 
 if($_SERVER['HTTP_HOST'] == 'localhost'){
 	$version = time();
